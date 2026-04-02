8a5d7e157d1370bb423aa7cc6573afeb6ac9d84f
Zalam2004 <dal638779@utdallas.edu>
2026-04-02 19:27:02 +0000

    Added rotation function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,35 @@
 // main.js
 
-console.log("Hello, World!");
-let rotation = 0;
-document.addEventListener("click", funtion() {
-    let min = 10;
-    let max = 40;
-    rotation += Math.floor(Math.random() * (max - min + 1)) + min;
-    document.body.style.transform = 'rotate(${rotation}deg)';
-});
+let grid = document.querySelectorAll(".grid-container");
+let divs = document.querySelectorAll(".grid-container div");
 
-/*function randomNumber(min, max) {
+/*let rotation = 0;
+document.addEventListener("click", function() {
+    //let min = 10;
+    //let max = 40;
+    rotation += randomNumber(1, 200);
+    document.body.style.transform = 'rotate(${rotation}deg)';
+}); */
+
+function randomize() {
+    console.log(divs.length, "randomize!");
+
+    grid[0].style.transform = `rotate(${Math.random() * 360}deg)`;
+
+    divs.forEach(function(div) {
+
+    let scale = randomNumber(.5, 1.2);
+    let translateX = randomNumber(0, 50);
+    let translateY = randomNumber(0, 20);
+    let rotate = randomNumber(0, 360);
+
+    div.style.transform = 'scale(${scale})translate(${translateX}%, ${translateY}%)rotate(${rotate}deg)';
+    })
+    
+}
+document.addEventListener("click", randomize);
+
+function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + 1) + min;
 }
-
-let divs = document.querySelectorAll(".grid-container div");
-function randomize() {
-    divs.forEach(function(div) {
-        let scale = randomNumber(.5, 1.2);
-        let translateX = randomNumber(0, 50);
-        let translateY = randomNumber(0, 20);
-        let rotate = randomNumber(0, 360);
-        div.computedStyleMap.transform = 
-        scale(${scale})
-        translate(${translateX}%, ${translateY}%)
-        rotate(${rotate}deg)';'
-    })
-    console.log(divs.length, "randomize!");
-}
-
-//randomize(); 
-document.addEventlistener("click", randomize);*/
+        
